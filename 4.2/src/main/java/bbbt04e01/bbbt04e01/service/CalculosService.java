f009bf44d9dff33c81420a76ddde6fbd19f26144
Marcos <marcosgp142@gmail.com>
2024-10-29 21:50:55 +0100

    Ejercicio 2, modificado el GetMapping para usar todos Post

--- 4.2/src/main/java/bbbt04e01/bbbt04e01/service/CalculosService.java
+++ 4.2/src/main/java/bbbt04e01/bbbt04e01/service/CalculosService.java
@@ -1,40 +1,13 @@
 package bbbt04e01.bbbt04e01.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
+public interface CalculosService {
 
-@Service
-public class CalculosService {
+    public boolean esPrimo(int numero);
 
-    public boolean esPrimo(int numero) {
-        if (numero <= 1) {
-            return false;
-        }
-       
-        for (int i = 2; i < numero; i++) {
-            if (numero % i == 0) { 
-                return false;
-            }
-        }
-    
-        return true;
-    }
+    public double calcularHipotenusa(double cateto1, double cateto2);
 
-    public double calcularHipotenusa(double cateto1, double cateto2) {
-        return Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
-    }
-
-    public List<Integer> obtenerDivisores(int numero) {
-        List<Integer> divisores = new ArrayList<>();
-        for (int i = 1; i <= numero; i++) {
-            if (numero % i == 0) {
-                divisores.add(i);
-            }
-        }
-        return divisores;
-    }
-    
+    public List<Integer> obtenerDivisores(int numero);
 
 }
