9fdf2c2752b7f0a2f1afb05eb972193bde4224eb
Marcos <marcosgp142@gmail.com>
2024-10-29 21:31:44 +0100

    Lo bueno

---
+++ 4.2/src/test/java/bbbt04e01/bbbt04e01/Bbbt04e01ApplicationTests.java
@@ -0,0 +1,13 @@
+package bbbt04e01.bbbt04e01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bbbt04e01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
