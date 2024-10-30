0d94c37be88cce08a50825f5af83de247ff43d89
BraisBB <becerra.brais@gmail.com>
2024-10-30 21:38:35 +0100

    Para entregar

--- Tema4/e01/src/main/java/com/t04/e01/service/CalculosService.java
+++
@@ -1,9 +0,0 @@
-package com.t04.e01.service;
-
-import java.util.List;
-
-public interface CalculosService {
-    boolean esPrimo(int numero);
-    double calcularHipotenusa(int cateto1, int cateto2);
-    List<Integer> obtenerDivisores(int numero);
-}
